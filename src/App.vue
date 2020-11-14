9ad33cd03d46590b0948483dc8c93e08964ff6e2
seiya0429 <seiya.060429@gmail.com>
2020-11-14 16:41:16 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,35 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Counter name="Counter 1" :initCount="5" @emitUp="getEvent" />
+    <Counter name="Counter 2" :initCount="10" @emitUp="getEvent" />
+
+    <p>
+      Event:
+      {{stack}}
+    </p>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter
+  },
+  data() {
+    return {
+      stack: []
+    }
+  },
+  methods: {
+    getEvent(payload) {
+      this.stack.push(payload)
+    }
   }
 }
 </script>
