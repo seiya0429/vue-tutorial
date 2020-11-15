dc8ad0cac1670fad00e2c0c80bb6ddc92ae931e5
seiya0429 <seiya.060429@gmail.com>
2020-11-15 13:24:55 +0900

    vue-router　チュートリアル

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Counter name="Counter 1" :initCount="5" @emitUp="getEvent" />
-    <Counter name="Counter 2" :initCount="10" @emitUp="getEvent" />
-
-    <p>
-      Event:
-      {{stack}}
-    </p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/count">Count</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import Counter from './components/Counter'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Counter
-  },
-  data() {
-    return {
-      stack: []
-    }
-  },
-  methods: {
-    getEvent(payload) {
-      this.stack.push(payload)
-    }
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/Paging.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <router-link :to="`/paging/${prevPage}`">Prev</router-link> |
+    <router-link :to="`/paging/${nextPage}`">Next</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    prevPage() {
+      return Number(this.$route.params.page) - 1
+    },
+    nextPage() {
+      return Number(this.$route.params.page) + 1
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
